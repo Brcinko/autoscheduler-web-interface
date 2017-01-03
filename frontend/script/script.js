ba8b7b6fba7f5d2c4fd907768f6d52aebf2c4558
Brcinko <lukas.klescinec@gmail.com>
2017-01-03 22:32:47 +0100

    added script for confs under development with bug - cant find element by document.getElementById()

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,4 +1,11 @@
-{
+	console.log("idem robit request2");
+	/*var request = XMLHttpRequest();
+	request.open("GET", "http://localhost:8080/get_conf", true);
+	request.send(null);
+	print(request.responseText);*/
+	//toto sa jaksik nefunguje zatial to musime zistit co s tym
+	//toto pod tym je vramci vyvoja
+	var obj = {
 	"_id": 1,
     "settings": [
             {
@@ -27,7 +34,22 @@
             }
     ],
 	"meta" : {
-		"date" : ISODate("2014-10-01T00:00:00Z"),
+		"date" : "2014-10-01T00:00:00Z",
 		"doc_version" : "0.2"
+	}
+};
+
+window.onload = function() {
+	for (k in obj.settings) {
+	   console.log(k, obj.settings[k].conf_status);
+	   //change status clmn in table
+	   var td_class = ".td-" + obj.settings[k].conf_status;
+	   console.log(td_class);
+	   document.getElementById(obj.settings[k].filter_name).className = td_class;
+	   console.log("mame classu");
+	   document.getElementById(obj.settings[k].filter_name).innerHTML = obj.settings[k].conf_status;
+	   
+
+
 	}
 }
